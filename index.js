92d61079a7650e7f9ade63e3a8d01a1dfe9bae92
farel <farelnanda11@gmail.com>
2024-06-14 23:49:01 +0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/groqai/:content', async (req, res) => {
   const reply = await groqai(req.params.content)
-  res.send(JSON.stringify({'data': reply}))
+  res.send(JSON.stringify(reply))
 })
 
 app.listen(port, () => {
